e1e3373713e0eebd9c5284ac1cabdcd568447063
Johan Gomez <johangomez0612@gmail.com>
2025-07-03 00:24:57 +0000

    generador de excusas terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ['My dog', 'My grandma', 'My dad', 'My brother', 'My mom']
+  let whoRandom = who[Math.floor(Math.random() * who.length)]
+
+  let action = ['Broke', 'Destroy', 'Burn',]
+  let actionRandom = action[Math.floor(Math.random() * action.length)]
+
+  let what = ['My phone', 'My car', 'My laptop']
+  let whatRandom = what[Math.floor(Math.random() * what.length)]
+
+  let when = ['Before the class', 'In the backyard', 'during my lunch', 'while I was praying', 'when I was sleeping']
+  let whenRandom = when[Math.floor(Math.random() * when.length)]
+
+  const excuse = `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`
+  document.getElementById("excuse").innerText = excuse
+  console.log(excuse)
+
 };
