c46b34906438bb876d897564477c865ffa76588e
Johan Gomez <johangomez0612@gmail.com>
2025-07-08 00:40:54 +0000

    modificación para reutilizar código con una función

--- src/app.js
+++ src/app.js
@@ -8,17 +8,22 @@ import "./assets/img/4geeks.ico";
 window.onload = function () {
   //write your code here
 
+  function generateRandomValue (array) {
+     
+    return array[Math.floor(Math.random() * array.length)]
+  }
+
   let who = ['My dog', 'My grandma', 'My dad', 'My brother', 'My mom']
-  let whoRandom = who[Math.floor(Math.random() * who.length)]
+  let whoRandom = generateRandomValue(who)
 
   let action = ['Broke', 'Destroy', 'Burn',]
-  let actionRandom = action[Math.floor(Math.random() * action.length)]
+  let actionRandom = generateRandomValue(action)
 
   let what = ['My phone', 'My car', 'My laptop']
-  let whatRandom = what[Math.floor(Math.random() * what.length)]
+  let whatRandom = generateRandomValue(what)
 
   let when = ['Before the class', 'In the backyard', 'during my lunch', 'while I was praying', 'when I was sleeping']
-  let whenRandom = when[Math.floor(Math.random() * when.length)]
+  let whenRandom = generateRandomValue(what)
 
   const excuse = `${whoRandom} ${actionRandom} ${whatRandom} ${whenRandom}`
   document.getElementById("excuse").innerText = excuse
